bd45e69687d6ed5830bbe40a7a203d454b1a1972
Steven-Dev <107848175+Stv-devl@users.noreply.github.com>
2022-08-09 08:24:31 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,66 @@
+//variables
 const form = document.querySelector("form");
-const ul = document.querySelector("ul");
 const bills = document.getElementById("bill");
 const peoples = document.getElementById("peoples");
 const buttons = document.querySelectorAll(".button");
-let number, numberT, number_people, bill, totalP;
+let calcTip,
+  calcPeople,
+  number_people,
+  bill,
+  totalTip,
+  totalPeople,
+  calcTipCustom,
+  calcPeopleCustom;
 
+//tips buttons & calc
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    number = e.target.id / 100;
-    numberT = 1 + e.target.id / 100;
+    calcTip = e.target.id / 100;
+    calcPeople = 1 + e.target.id / 100;
   });
 });
-
-peoples.addEventListener("input", (e) => {
-  number_people = e.target.value;
-  console.log(number_people);
+//custom tip
+custom.addEventListener("change", (e) => {
+  calcTipCustom = e.target.value / 100;
+  calcPeopleCustom = 1 + e.target.value / 100;
 });
-
-bills.addEventListener("input", (e) => {
+//number of people
+peoples.addEventListener("change", (e) => {
+  number_people = e.target.value;
+});
+//bill price
+bills.addEventListener("change", (e) => {
   bill = e.target.value;
 });
-
-form.addEventListener("input", (e) => {
-  if ((number_people > 0, bill > 0, number > 0)) {
-    total = (bill * number) / number_people;
-    totalP = (bill * numberT) / number_people;
-    total = total.toFixed(2);
-    totalP = totalP.toFixed(2);
-    amountP.textContent = `$${total}`;
-    amountT.textContent = `$${totalP}`;
-    console.log(total);
-  } else {
-    amountP.textContent = `0.00`;
+//if bill, tip btn, number selected => calc => display tip amount and Total to pay by person
+form.addEventListener("change", () => {
+  if (number_people > 0 && bill > 0 && calcTip > 0) {
+    totalTip = ((bill * calcTip) / number_people).toFixed(2); //tip amount
+    totalPeople = ((bill * calcPeople) / number_people).toFixed(2); //total price person
+    amountTip.textContent = `$${totalTip}`;
+    amountTotal.textContent = `$${totalPeople}`;
   }
+  //if bill, tip custom, number selected => calc => display tip amount and Total to pay by person
+  if (calcTipCustom > 0 && number_people > 0 && bill > 0) {
+    totalTip = ((bill * calcTipCustom) / number_people).toFixed(2); //tip amount
+    totalPeople = ((bill * calcPeopleCustom) / number_people).toFixed(2); //total price person
+    amountTip.textContent = `$${totalTip}`;
+    amountTotal.textContent = `$${totalPeople}`;
+  }
+});
+
+//reset button
+reset.addEventListener("click", (e) => {
+  bill = 0;
+  number_people = 0;
+  calcTip = 0;
+  calcPeople = 0;
+  calcTipCustom = 0;
+  calcPeopleCustom = 0;
+  bills.value = "";
+  peoples.value = "";
+  buttons.value = 0;
+  custom.value = "";
+  amountTip.textContent = "$0.00";
+  amountTotal.textContent = "$0.00";
 });
